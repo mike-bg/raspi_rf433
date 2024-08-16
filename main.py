8b8098e86828a2121aff28a334ba52ba7b1702f3
mike-bg <mbergesgomez@gmail.com>
2024-08-17 00:58:45 +0200

    prueba

---
+++ main.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+from piVirtualWire import piVirtualWire
+
+
+# Configura el pin GPIO donde está conectado el receptor
+RF_PIN = 17
+
+# Inicializa PiVirtualWire
+vw = piVirtualWire(tx_pin=None, rx_pin=RF_PIN, speed=2000)  # 2000 bps como en el Arduino
+
+def receive_rf_signal():
+    print("Esperando señal RF...")
+    while True:
+        msg = vw.get_message()  # Intenta recibir un mensaje
+        if msg:
+            print("Mensaje recibido:", msg.decode('utf-8'))  # Imprime el mensaje recibido
+
+if __name__ == "__main__":
+    try:
+        receive_rf_signal()
+    except KeyboardInterrupt:
+        print("Finalizando...")
+    finally:
+        GPIO.cleanup()
